c38322e9bd5bca9083533805504eac82a977dcef
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-03-27 21:59:04 +0200

    Register Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './INDEX/landing/landing.component';
 import { LoginComponent } from './INDEX/login/login.component';
+import { RegisterComponent } from './INDEX/register/register.component';
 
 const routes: Routes = [
   {path:'',component:LandingComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent}
+
 ];
 
 @NgModule({
