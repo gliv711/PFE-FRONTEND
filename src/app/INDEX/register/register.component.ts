be9084ab126d8a9df5350500b56464bb1e11df94
hedhlikhalil <123672986+hedhlikhalil@users.noreply.github.com>
2023-04-02 15:41:37 +0100

    l page lkol mtaa login jawhom behy
    update

--- src/app/INDEX/register/register.component.ts
+++ src/app/INDEX/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { 
+  constructor(private router: Router)  { 
     
     const n :number=0
     
@@ -18,6 +19,9 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  Onclick(){
+    this.router.navigate(['/accordion']);
+}
   
 
 }
--- src/app/Platform/dashboard/addons/badges/badges.component.ts
+++ src/app/Platform/dashboard/addons/badges/badges.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-badges',
   templateUrl: './badges.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BadgesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
   }
-
+  Onclick(){
+    this.router.navigate(['/badges']);
+}
 }
