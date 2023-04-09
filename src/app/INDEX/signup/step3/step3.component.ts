0b948ebf1300b61db703880745f7acec850d8663
hedhlikhalil <123672986+hedhlikhalil@users.noreply.github.com>
2023-04-09 03:28:43 +0100

    step3 validations

---
+++ src/app/INDEX/signup/step3/step3.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-step3',
+  templateUrl: './step3.component.html',
+  styleUrls: ['./step3.component.css']
+})
+export class Step3Component implements OnInit {
+  myform : any;
+
+  constructor(private router: Router ,private formbuilder : FormBuilder) { 
+    this.myform = this.formbuilder.group({
+      universite :['', [Validators.required, Validators.minLength(3), Validators.pattern('[a-zA-Z ]*'), Validators.maxLength(30)]],
+      domaine:['', [Validators.required, Validators.minLength(3), Validators.pattern('[a-zA-Z ]*'), Validators.maxLength(30)]]
+
+    })
+
+  }
+
+  ngOnInit(): void {
+  }
+  Onclick():any {
+    if (this.myform.valid) {
+      this.router.navigate(['/step4']);
+    }
+  }
+
+}
