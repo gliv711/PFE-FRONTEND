f767258bc01660265a8eef8cc4a16d74646e8f4b
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-13 21:26:51 +0200

    Update Questions.ts

--- src/Models/Questions.ts
+++ src/Models/Questions.ts
@@ -1,11 +1,9 @@
 import { Answer } from "./Answer";
-import { Survey } from "./Survey";
 
 export interface Questions {
   question_id?: number;
     question?: string;
     field?: string;
     answers?: Answer[];
-    questions?:Questions[];
-    surveys?:Survey[];
+   
   }
