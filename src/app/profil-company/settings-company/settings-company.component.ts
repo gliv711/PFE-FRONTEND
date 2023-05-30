2eb926aaf24aceb7caf365cec11ad6c691d96804
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-30 14:47:13 +0200

    Update settings-company.component.ts

--- src/app/profil-company/settings-company/settings-company.component.ts
+++ src/app/profil-company/settings-company/settings-company.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from 'src/Models/Users/User';
+import { UserServiceService } from 'src/Services/user-service/user-service.service';
 
 @Component({
   selector: 'app-settings-company',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsCompanyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService : UserServiceService) { }
 
   ngOnInit(): void {
+    const email: string = this.getemail(); // Assuming 'getemail()' returns a string
+    this.getUserByEmail(email); // Passing the 'email' variable instead of 'this.email'
   }
 
+  user: User = {
+    name: '',
+    lastName: '',
+    email: '',
+    phone_number: '',
+    address: '',
+    region: '',
+    university: '',
+    domain: ''
+  };
+
+  getUserByEmail(email: string): void {
+    this.userService.getUserByEmail(email).subscribe({
+      next: (user: User) => {
+        this.user = user;
+        this.user.domain = user.domain;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  helper = new JwtHelperService();
+
+  getemail(): string {
+    const accessToken: any = localStorage.getItem('accesstoken');
+    const decodeAccessToken = this.helper.decodeToken(accessToken);
+    console.log(decodeAccessToken.sub);
+    return decodeAccessToken.sub;
+  }
 }
