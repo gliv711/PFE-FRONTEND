bc51298635c81641b83583a1bd2ff4defcc7b1ab
hedhlikhalil <123672986+hedhlikhalil@users.noreply.github.com>
2023-05-30 15:33:22 +0100

    service

--- src/Models/Users/Company.ts
+++ src/Models/Users/Company.ts
@@ -3,10 +3,10 @@ import { lambda } from "./Lambda";
 
 
 export class Company extends lambda {
-    NameofCompany?:string;
-    DomaineofActivite?:string;
-    NameofResponsible?:string;
-    SocialReason?:string;
+    nameofCompany?:string;
+    domaineofActivity?:string;
+    nameofResponsible?:string;
+    socialReason?:string;
     link?:File;
   
     constructor(id: number, email: string, Password: string,phone_number: string,address:string,image : string) {
