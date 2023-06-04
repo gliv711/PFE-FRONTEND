9a0a430c008bbe948c9334a07e80350e1e787d76
hedhlikhalil <123672986+hedhlikhalil@users.noreply.github.com>
2023-06-04 23:00:14 +0100

    module user

---
+++ src/app/profil-user/profil-user.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfilComponent } from './profil/profil.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AcceuilUserComponent } from './acceuil-user/acceuil-user.component';
+import { NavbarUserComponent } from './navbar-user/navbar-user.component';
+import { OffreUserComponent } from './offre-user/offre-user.component';
+import { SettingsUserComponent } from './settings-user/settings-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProfilComponent,
+    AcceuilUserComponent,
+    NavbarUserComponent,
+    OffreUserComponent,
+    SettingsUserComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    AppRoutingModule,
+
+  ],
+  exports : [ProfilComponent]
+})
+export class ProfilUserModule { }
