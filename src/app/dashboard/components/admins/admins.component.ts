9bb45e240a56bae12e5641aad9a9218898d43b72
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-06-13 14:00:07 +0200

    n

--- src/app/dashboard/components/admins/admins.component.ts
+++ src/app/dashboard/components/admins/admins.component.ts
@@ -7,6 +7,7 @@ import { Role } from 'src/enums/role.enum';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmailValidator } from 'src/email controle/EmailValidator';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-admins',
@@ -19,12 +20,13 @@ export class AdminsComponent implements OnInit {
   admin : admin = {};
 
 
+  currentFile : File;
 
   chargement =false ;
   mise_a_jour=false ;
   supprimer=false ;
   error = false ;
-  constructor(private UserService : UserServiceService ,private authservice :AuthService,private router:Router,private formBuilder: FormBuilder,private emailValidator: EmailValidator) {
+  constructor(private UserService : UserServiceService ,private authservice :AuthService,private router:Router,private formBuilder: FormBuilder,private emailValidator: EmailValidator,private sanitizer: DomSanitizer) {
     
     this.myForm = this.formBuilder.group({
       address: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]],
@@ -100,31 +102,73 @@ export class AdminsComponent implements OnInit {
     
   }
 
-  AddUser() {
-    if (this.myForm.invalid) {
-      return;
-    }
-    const { phone_number, password, address, email } = this.myForm.value;
-    const user = { phone_number, password, address, email };
-    this.UserService.addadmin(user).subscribe({
+  // AddUser() {
+  //   if (this.myForm.invalid) {
+  //     return;
+  //   }
+  //   const { phone_number, password, address, email } = this.myForm.value;
+  //   const user = { phone_number, password, address, email };
+  //   this.UserService.addadmin(user).subscribe({
       
-      next: () => {
+  //     next: () => {
        
+  //       this.getAdmins();
+  //       this.mise_a_jour = true; 
+  //       setTimeout(() => {
+  //         this.mise_a_jour = false;
+  //       }, 3000); 
+  //     },
+  //     error: (e) => {
+  //       console.log(e);
+  //       this.error = true;
+  //     },
+  //     complete: () => {
+  //       this.close();
+  //     }
+  //   });
+    
+  // }
+
+  onFileSelected(event : any) {
+
+    this.currentFile = event.target.files[0];
+    console.log("file selected")
+}
+getSrcFromCustomFile(admin : admin) {
+  let uint8Array = new Uint8Array(atob(admin.picture.data).split("").map(char => char.charCodeAt(0)));
+  let dwn = new Blob([uint8Array])
+  return this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(dwn));
+}
+  AddAdmin(admin:admin){
+    const formData : FormData = new FormData();
+
+    if (admin.id!=null)
+    formData.append('id', admin.id+"");
+    formData.append('email', admin.email+"");
+    formData.append('address', admin.address+"");
+    formData.append('phone_number', admin.phone_number+"");
+    formData.append('password', admin.password+"");
+    if (this.currentFile != null) {
+      formData.append('picture_file',this.currentFile,this.currentFile?.name);
+    }
+
+    this.UserService.addAdmin(formData).subscribe({
+      next: () => {
         this.getAdmins();
-        this.mise_a_jour = true; 
+        this.mise_a_jour=true; 
         setTimeout(() => {
           this.mise_a_jour = false;
         }, 3000); 
       },
-      error: (e) => {
-        console.log(e);
-        this.error = true;
-      },
+      error: (e) =>  {console.log(e),this.error=true;},
       complete: () => {
-        this.close();
+        
+
       }
-    });
-    
+    })
+
+
+   
   }
   
   }
